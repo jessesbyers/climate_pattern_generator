075dad525371f16c4b302c2eb16e991dfb7f03d8
Jesse Byers <jesse.s.byers@gmail.com>
2019-10-10 12:40:00 +0000

    repaired interface loops so exit is now working

--- lib/climate_pattern_generator/cli.rb
+++ lib/climate_pattern_generator/cli.rb
@@ -3,9 +3,54 @@ class ClimatePatternGenerator::CLI
 
   def call
     puts "Welcome to the Climate Pattern Generator"
-    menu
+    options
+    menu_loop
     # list_data
     goodbye
+  end
+
+  def options
+    puts "What would you like to do?"
+    puts "1. Create a pattern by year"
+    # ## will build for option 1, then add more features later
+    # puts "2. Create a pattern by location"
+    # puts "3. Create a pattern by year and location"
+    puts "Type a number to make your choice."
+  end
+
+  def menu_loop
+    input = nil
+
+    while input != "exit"
+      puts "Type exit or back at any time."
+      input = gets.strip
+      case input
+
+      when "1"
+        puts "Choose a year between 1900 and 2018"
+          input = gets.strip
+        #add logic for choosing year
+        list_data
+        puts "================ see data above ========================"
+
+
+
+      when "2"
+        puts "Please come back soon - this option is not yet available"
+
+
+      when "3"
+        puts "Please come back soon - this option is not yet available"
+
+
+      else
+        options
+      end
+    end
+  end
+
+  def goodbye
+    puts "Thank you for using the Climate Pattern Generator"
   end
 
   def list_data
@@ -19,52 +64,6 @@ class ClimatePatternGenerator::CLI
     puts "_________   #{date}   #{temperature}   #{color}    #{url}"
   end
 
-  def menu
-    puts "What would you like to do?"
-    puts "1. Create a pattern by year"
-    # ## will build for option 1, then add more features later
-    # puts "2. Create a pattern by location"
-    # puts "3. Create a pattern by year and location"
-    puts "Type a number to make your choice. Type exit or back at any time."
-    input = gets.strip
 
-    while input != "exit"
-      if input == "1"
-        puts "Choose a year between 1900 and 2018"
-        input = gets.to_i
-        while input != "exit"
-
-          if input >= 1900 && input <= 2018
-            puts  "get_pattern_by_year method"
-            puts "list_data method"
-          else
-            puts "Try again."
-            menu
-          end
-        end
-
-      elsif input == "2"
-        puts "Please come back soon - this option is not yet available"
-        menu
-
-      elsif input == "3"
-        puts "Please come back soon - this option is not yet available"
-        menu
-
-      # elsif input == "exit"
-      #   puts "Goodbye!"
-      else
-        menu
-      end
-
-      if input == "exit"
-        puts "Goodbye!"
-      end
-    end
-  end
-
-  def goodbye
-    puts "Thank you for using the Climate Pattern Generator"
-  end
 
 end

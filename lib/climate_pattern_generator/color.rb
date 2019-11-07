a2de2905561afc44827a66d626be503e2af490ca
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-07 18:32:54 +0000

    color.new_from_db added

--- lib/climate_pattern_generator/color.rb
+++ lib/climate_pattern_generator/color.rb
@@ -6,7 +6,16 @@ class ClimatePatternGenerator::Color
     @color = color
     @min = min
     @max = max
-    @@all << self
+    # @@all << self
+  end
+
+  def self.new_from_db(row)
+    new_color = self.new
+    new_color.id = row[0]
+    new_color.color = row[1]
+    new_color.min = row[2]
+    new_color.max = row[3]
+    new_color
   end
 
   def self.all

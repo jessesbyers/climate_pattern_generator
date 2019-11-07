88c99064ccaf9a1f692138a19cfb9d5830a1e925
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-07 16:27:55 +0000

    color.new revised to include id

--- lib/climate_pattern_generator/color.rb
+++ lib/climate_pattern_generator/color.rb
@@ -1,11 +1,13 @@
 class ClimatePatternGenerator::Color
   attr_accessor :color, :min, :max
+  attr_reader :id
   @@all = []
 
-  def initialize(color, min, max)
+  def initialize(color, min, max, id=nil)
     @color = color
     @min = min
     @max = max
+    @id = id
     @@all << self
   end
 

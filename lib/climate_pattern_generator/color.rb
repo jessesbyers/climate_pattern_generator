2a3779d18f57b5d2189d6841798794abd7769602
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-07 16:34:35 +0000

    added Color.save and Color.create methods

--- lib/climate_pattern_generator/color.rb
+++ lib/climate_pattern_generator/color.rb
@@ -8,7 +8,7 @@ class ClimatePatternGenerator::Color
     @min = min
     @max = max
     @id = id
-    @@all << self
+    # @@all << self
   end
 
   def self.all
@@ -16,16 +16,35 @@ class ClimatePatternGenerator::Color
   end
 
   def self.create_table
-  sql =  <<-SQL
-    CREATE TABLE IF NOT EXISTS colors (
-      id INTEGER PRIMARY KEY,
-      color TEXT,
-      min INTEGER,
-      max INTEGER
-      )
-      SQL
-  DB[:conn].execute(sql)
-end
-end
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS colors (
+        id INTEGER PRIMARY KEY,
+        color TEXT,
+        min INTEGER,
+        max INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+   sql = <<-SQL
+     INSERT INTO colors (id, color, min, max)
+     VALUES (?, ?, ?, ?)
+   SQL
+
+   DB[:conn].execute(sql, self.color, self.min, self.max)
+
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+ end
+
+ def self.create(color, min, max)
+   color = Color.new(color, min, max)
+   color.save
+   color
+ end
+
+
+
 
 end
